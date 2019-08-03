96348b03571e920dcd79456180b63462ebb48436
kirub <kirubb@gmail.com>
2019-08-04 00:01:48 +0100

    Add first draft real map Add Planet Randomizer

---
+++ GMTK2019/Assets/Src/World/PlanetRandomizerComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRandomizerComponent : MonoBehaviour
+{
+    public List<GameObject>    PlanetPrefabs = new List<GameObject>();
+
+    private void Awake()
+    {
+        int Index = Random.Range(0, PlanetPrefabs.Count);
+        Instantiate(PlanetPrefabs[Index], transform);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
