35bf03ad2b444e9a95cc919f16ac163eebbd270e
kirub <kirubb@gmail.com>
2019-08-03 05:22:10 +0100

    First draft orbital component

---
+++ GMTK2019/Assets/Src/Star/OrbitalComponent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalComponent : MonoBehaviour
+{
+
+    public float Mass { get; private set; }
+    public float AttractionMass { get; private set; }
+    public GameObject _AttractedBy;
+    public GameObject AttractedBy {
+        get
+        {
+            return _AttractedBy;
+        }
+        set
+        {
+            _AttractedBy = value;
+        }
+    }
+    [SerializeField]
+    private bool IsAStaticStar = false;
+    public bool IsStatic { get { return IsAStaticStar; } }
+
+    private void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*AttractedBy =*/ PlanetManager.Instance.RegisterPlanet(gameObject);
+        gameObject.transform.SetParent(AttractedBy.gameObject.transform, true);
+        AttractionMass = gameObject.GetComponent<Rigidbody>().mass + AttractedBy.gameObject.GetComponent<Rigidbody>().mass;
+        if (!AttractedBy)
+        {
+            Debug.Log("no Attracted by");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AttractedBy)
+        {
+            float Dist = Vector3.Distance(AttractedBy.transform.position, gameObject.transform.position);
+            /*
+            float k = (4 * Mathf.PI * Mathf.PI) / ;
+            //float PeriodSq = k * Mathf.Pow(Dist, 3);
+            float PeriodSq = k * Dist * Dist;*/
+            float Speed = Time.deltaTime * 300 /Dist;// Mathf.Sqrt(PlanetManager.G * AttractionMass / Dist);
+            
+            gameObject.transform.RotateAround(AttractedBy.transform.position, Vector3.up, Speed);
+        }
+    }
+
+
+
+    public static float ComputeAttraction(OrbitalComponent Lhs, OrbitalComponent Rhs)
+    {
+        Rigidbody LhsRBody = Lhs.gameObject.GetComponent<Rigidbody>();
+        Rigidbody RhsRBody = Rhs.gameObject.GetComponent<Rigidbody>();
+        float Distance = Vector3.Distance(Lhs.gameObject.transform.position, Rhs.gameObject.transform.position);
+        float m1m2      = LhsRBody.mass * RhsRBody.mass;
+        return PlanetManager.G * m1m2 / (Distance * Distance);
+    }
+
+    public static bool CompareAttraction( KeyValuePair<OrbitalComponent, OrbitalComponent> Lhs, KeyValuePair<OrbitalComponent, OrbitalComponent> Rhs)
+    {
+        return ComputeAttraction(Lhs.Key, Lhs.Value) < ComputeAttraction(Rhs.Key, Rhs.Value);
+    }
+}
---
+++ GMTK2019/Assets/Src/Star/PlanetManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public static float G = 9.81f;
+
+    public static PlanetManager Instance { get; private set; } = null;
+
+    public ArrayList Planets { get; } = new ArrayList();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject FindAttractedPlanet(GameObject Planet )
+    {
+        OrbitalComponent AttractedByComponent = null;
+        OrbitalComponent LastComponent = null;
+        OrbitalComponent AddedOrbitalComp = Planet.GetComponent<OrbitalComponent>();
+        foreach (GameObject PlanetObj in Planets)
+        {
+            OrbitalComponent OrbitalComp = PlanetObj.GetComponent<OrbitalComponent>();
+            if (OrbitalComp)
+            {
+                bool IsAttracted = false;
+                KeyValuePair<OrbitalComponent, OrbitalComponent> Pair1;
+                KeyValuePair<OrbitalComponent, OrbitalComponent> Pair2;
+                if (!AddedOrbitalComp.IsStatic && LastComponent)
+                {
+                    Pair1 = new KeyValuePair<OrbitalComponent, OrbitalComponent>(AddedOrbitalComp, LastComponent);
+                    Pair2 = new KeyValuePair<OrbitalComponent, OrbitalComponent>(AddedOrbitalComp, OrbitalComp);
+
+                    if (OrbitalComponent.CompareAttraction(Pair1, Pair2))
+                    {
+                        IsAttracted = true;
+                        AttractedByComponent = OrbitalComp;
+                        LastComponent = AttractedByComponent;
+                    }
+                    else if (!AttractedByComponent)
+                    {
+                        AttractedByComponent = LastComponent;
+                    }
+                }
+
+                if (!IsAttracted && !OrbitalComp.IsStatic)
+                {
+                    if (OrbitalComp.AttractedBy)
+                    {
+                        Pair1 = new KeyValuePair<OrbitalComponent, OrbitalComponent>(OrbitalComp, OrbitalComp.AttractedBy.GetComponent<OrbitalComponent>());
+                        Pair2 = new KeyValuePair<OrbitalComponent, OrbitalComponent>(OrbitalComp, AddedOrbitalComp);
+
+                        if (OrbitalComponent.CompareAttraction(Pair1, Pair2))
+                        {
+                            OrbitalComp.AttractedBy = AddedOrbitalComp.gameObject;
+                        }
+                    }
+                    else
+                    {
+                        OrbitalComp.AttractedBy = AddedOrbitalComp.gameObject;
+                    }
+                }
+
+                if (!LastComponent)
+                {
+                    LastComponent = OrbitalComp;
+                }
+            }
+        }
+
+        return AttractedByComponent.gameObject;
+    }
+
+    public GameObject RegisterPlanet( GameObject Planet )
+    {
+        GameObject AttractedByObj = null;
+        //AttractedByObj = FindAttractedPlanet(Planet);
+        Planets.Add(Planet);
+        return AttractedByObj;
+    }
+}
