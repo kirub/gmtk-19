61a6296ef2cea00bf6f18c0131b6808d3838125f
kirub <kirubb@gmail.com>
2019-08-03 22:00:29 +0100

    Add CollisionCheckComponent Exception List (with Nova) Add FPP Scenes Tests

--- GMTK2019/Assets/Src/Ship/CollisionCheckComponent.cs
+++ GMTK2019/Assets/Src/Ship/CollisionCheckComponent.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionCheckComponent : MonoBehaviour
-{	
+{
+    [SerializeField]
+    List<string> CollisionsExceptions = new List<string>();
+
+    private List<string> CollisionTagExceptionsList { get { return CollisionsExceptions; } }
+
 	private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("OrbitalInnerRadius") || other.CompareTag("OrbitalOuterRadius"))
+        if (CollisionTagExceptionsList.Contains(other.tag))
             return;
 
         if (!ShipUnit.Instance)
