156fd2f1ed0d12c9a8449d90b7898a3e4ee03241
Gengler Corentin <gengcor@gmail.com>
2019-08-06 12:17:29 +0200

    sound when hovering button

---
+++ GMTK2019/Assets/Src/Tools/ButtonHoverSounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHoverSounds : MonoBehaviour
+{
+    public AudioSource Source;
+    public List<AudioClip> audioClips = new List<AudioClip>();
+    public void PlayRandomSound()
+    {
+        int NumSoundToPlay = Random.Range(0, audioClips.Count);
+        Source.PlayOneShot(audioClips[NumSoundToPlay]);
+        Debug.Log("sound" + NumSoundToPlay);
+    }
+}
