ffa0d66c387039a3427f262202840fd066c1c3c8
MaxiPoki <max.piquot@gmail.com>
2019-08-25 15:44:19 +0100

    [Debug] When not in editor all debug scripts destroy themselves [DebugTools] Add a component to reload the current scene [Prefab] Add DebugTools prefab that has a DebugDrawHelper and DebugCurrentSceneReloader

---
+++ GMTK2019/Assets/DebugTools/DebugCurrentSceneReloader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugCurrentSceneReloader : MonoBehaviour
+{
+	// -------------------------------------------------------------------------------------
+
+	[SerializeField] private KeyCode RestartKey = KeyCode.R;
+
+	// -------------------------------------------------------------------------------------
+
+	private void Awake()
+	{
+#if !UNITY_EDITOR
+		Destroy(this);
+#endif // UNITY_EDITOR
+	}
+
+	private void Update()
+	{
+#if UNITY_EDITOR
+		if (Input.GetKeyDown(RestartKey))
+		{
+			Debug.Log("Reload Current Scene");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+#endif // UNITY_EDITOR
+	}
+}
